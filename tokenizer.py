7f7bd5d18ac50a0b61fcec2d6b601d0d917713cc
ShedrachIkenna <Shedrach686@gmail.com>
2025-12-02 05:39:13 +0100

    lib imports

---
+++ tokenizer.py
@@ -0,0 +1,18 @@
+
+from logging import getLogger
+from pathlib import Path 
+
+# type hints for better code documentation and IDE support 
+from typing import (
+    AbstractSet,
+    Collection,
+    Dict,
+    Iterator,
+    List,
+    Literal,
+    Optional,
+    Sequence,
+    Union,
+    cast,
+)
+
