6e27d670d76924ff5617e3b858e80854f09e8fe1
ShedrachIkenna <Shedrach686@gmail.com>
2025-12-04 00:40:40 +0100

    defined constants in tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -18,3 +18,17 @@ from typing import (
 
 import tiktoken # OpenAI's fast BPE tokenizer implementation in Rust 
 
+from tokenizer_utils import load_bpe_file # get merge rules from the model file using the load_bpe_file function 
+
+logger = getLogger(__name__)
+
+# CONSTANTS 
+TIKTOKEN_MAX_ENCODE_CHARS = 400_000 # maximum characters tiktoken can encode 
+
+MAX_NO_WHITESPACES_CHARS = 25_000 # Maximum consecutive whitespace or non-whitespace characters before splitting
+
+_INSTANCE = None # Global variable for singleton pattern - stores and caches one tokenizer instance to avoid reloading 
+
+
+
+
