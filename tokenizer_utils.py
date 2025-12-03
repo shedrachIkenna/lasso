684efb804fe01ff631e997eb2e7a4208003ba257
ShedrachIkenna <Shedrach686@gmail.com>
2025-12-03 23:46:53 +0100

    added function description for load_bpe_file function

--- tokenizer_utils.py
+++ tokenizer_utils.py
@@ -4,3 +4,10 @@ import logging
 
 log = logging.getLogger(__name__)
 
+def load_bpe_file(model_path: Path) -> dict[bytes, int]:
+    """
+    Args: 
+        model_path (Path): Path to the BPE model file 
+        
+    Returns the merge rules stored in the model file 
+    """
