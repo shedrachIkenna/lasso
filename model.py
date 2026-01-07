6469a227e379eab96155eab231efda5780c4512a
ShedrachIkenna <Shedrach686@gmail.com>
2026-01-07 05:25:27 +0100

    Added rmsnorm function logic

--- model.py
+++ model.py
@@ -12,3 +12,8 @@ def rmsnorm(x, eps):
         # + eps: Add the safety constant to prevent division by zero error 
         # torch.rsqrt(...): calculate 1 / sqrt(value)
         # y * ...: Multiply the original vector by the reciprocal 
+        return y * torch.rsqrt(y.pow(2).mean(-1, keepdim=True) + eps)
+
+    # .float(): force to float32 for high precision during the math calculation 
+    # .type_as(x): Convert back to original type (eg. bfloat16) to save memory 
+    return _norm(x.float()).type_as(x)
