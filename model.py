02a01eb6057f9ea84ab055b09a5d4ecec9e913dd
ShedrachIkenna <Shedrach686@gmail.com>
2026-01-07 05:20:57 +0100

    Added rms comment calculation steps in rmsnorm function

--- model.py
+++ model.py
@@ -1,6 +1,14 @@
 import math 
-from typing import Any, Dict, List, Optional, Tuple 
-
-import fairscale.nn.model_parallel.initialize as fn_init 
+from typing import Any, Dict, Optional, Tuple
 import torch 
-import torch.nn.functional as F
+import torch.nn.functional as F 
+from torch import nn 
+
+
+def rmsnorm(x, eps):
+    def _norm(y):
+        # y.pow(2): Square every number in the vector 
+        # .mean(-1, keepdim=True): Average them across the last dimension 
+        # + eps: Add the safety constant to prevent division by zero error 
+        # torch.rsqrt(...): calculate 1 / sqrt(value)
+        # y * ...: Multiply the original vector by the reciprocal 
