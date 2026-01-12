88debe3e00d1ec881fac30a1d06a3e827a751ada
ShedrachIkenna <Shedrach686@gmail.com>
2026-01-12 07:24:42 +0100

    added rmsnorm logic correction

--- model.py
+++ model.py
@@ -4,17 +4,16 @@ import torch
 import torch.nn.functional as F 
 from torch import nn 
 
-
 def rmsnorm(x, eps):
     def _norm(y):
         return y * torch.rsqrt(y.pow(2).mean(-1, keepdim=True) + eps)
     return _norm(x.float()).type_as(x)
 
-class RMSNorm(torch.nn.Module):
+class RMSNorm(nn.Module):
     def __init__(self, dim: int, eps: float = 1e-6):
         super().__init__()
-        self.eps = eps
+        self.eps = eps 
         self.weight = nn.Parameter(torch.ones(dim))
-    
+
     def forward(self, x):
         return rmsnorm(x, self.eps) * self.weight
