c41d31b20b9cc47262b68a15afe729097527a237
ShedrachIkenna <Shedrach686@gmail.com>
2026-01-23 06:07:15 +0100

    Added model params in ModelArgs class

--- args.py
+++ args.py
@@ -9,4 +9,21 @@ class ModelArgs(BaseModel): # inherite all the methods from the basemodel class 
     n_kv_heads: Optional[int] = None 
     head_dim: Optional[int] = None 
 
+    vocab_size: int = -1 
+    multiple_of: int = 256 
+    ffn_dim_multiplier: Optional[float] = None 
+    ffn_exp: Optional[float] = None 
+    norm_eps: float = 1e-5
+
+    attention_chunk_size: Optional[int] = None 
+    rope_theta: float = 500000
+    use_scaled_rope: bool = False 
+    rope_scaling_factor: Optional[float] = None 
+    rope_high_freq_factor: Optional[float] = None 
+
+    max_batch_size: int = 32 
+    max_seq_len: int = 2048
+
+    
+
     
