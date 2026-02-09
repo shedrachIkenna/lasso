4e286412b8780fa96d99e77b00e86ba737af665c
ShedrachIkenna <Shedrach686@gmail.com>
2026-02-09 23:14:54 +0100

    added ModelArgs and FeedForward imports in moe.py

--- moe.py
+++ moe.py
@@ -3,3 +3,10 @@ import torch
 from torch import Tensor, nn 
 from torch.nn import functional as F 
 
+import fairscale.nn.model_parallel.initialize as fs_init 
+from fairscale.nn.model_parallel.mappings import reduce_from_model_parallel_region
+
+from .args import ModelArgs
+from .ffn import FeedForward
+
+
