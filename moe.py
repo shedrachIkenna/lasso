22003871215e686eda8873f4706e2275dd343656
ShedrachIkenna <Shedrach686@gmail.com>
2026-02-11 20:47:19 +0100

    Added init method of Experts class

--- moe.py
+++ moe.py
@@ -9,4 +9,43 @@ from fairscale.nn.model_parallel.mappings import reduce_from_model_parallel_regi
 from .args import ModelArgs
 from .ffn import FeedForward
 
+def divide_exact(numerator: int, denominator: int) -> int: 
+    assert numerator % denominator == 0, "{} is not divisible by {}".format(numerator, denominator)
+    return numerator // denominator
 
+class Experts(nn.Module):
+    def __init__(self, num_local_experts: int, dim: int, hidden_dim: int) -> None:
+        super().__init__()
+        dtype = torch.get_default_dtype()
+        self.num_local_experts = num_local_experts
+        self.dim = dim 
+        divide_factor = fs_init.get_model_parallel_world_size()
+
+        self.w1: nn.Parameter = nn.Parameter(
+            torch.empty(
+                num_local_experts, 
+                dim, 
+                divide_exact(hidden_dim, divide_factor),
+                dtype=dtype
+            )
+        )
+
+        self.w2: nn.Parameter = nn.Parameter(
+            torch.empty(
+                num_local_experts, 
+                dim,
+                divide_exact(hidden_dim, divide_factor),
+                dtype=dtype
+            )
+        )
+
+        self.w3: nn.Parameter = nn.Parameter(
+            torch.empty(
+                num_local_experts, 
+                dim, 
+                divide_exact(hidden_dim, divide_factor), 
+                dtype=dtype
+            )
+        )
+
+        self._register_load_state_dict_pre_hook(self.load_hook)
