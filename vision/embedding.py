47a3c1fdce3d0a9111b76167d990bfc3cc98f653
ShedrachIkenna <Shedrach686@gmail.com>
2026-03-08 12:28:08 +0100

    added PixelShuffle class in vision/embedding.py

--- vision/embedding.py
+++ vision/embedding.py
@@ -8,3 +8,20 @@ from fairscale.nn.model_parallel.layers import ColumnParallelLinear, RowParallel
 
 from ..args import VisionArgs
 
+class PixelShuffle(nn.Module): 
+    def __init__(self, ps_ratio):
+        super().__init__()
+        self.ps_ratio = ps_ratio
+
+    def forward(self, x): 
+        # x: [B, N, C] where N = number of patches 
+        # N: number of tokens (or seq)
+        # C: How many numbers are inside each patch (token embeddings - channels/embedding_dim)
+        assert self.ps_ratio is not None, "ps_ratio is required for pixel shuffle"
+        assert x.dim() == 3, "pixel shuffle requires encoded patches [B, N, C]"
+        # get the height and width of the image tensor: x 
+        hh = ww = int(math.sqrt(x.shape[1])) # x.shape[1] = N. so we take the sqrt of N which gives us the stacked hh and ww 
+        x = x.reshape(x.shape[0], hh, ww, -1) # x changes from [B, N, C] to [B, hh, ww, C]
+        x = pixel_shuffle_op(x, ps_ratio=self.ps_ratio) # [B, hh, ww, C] -> [B, hh/2, ww/2, 4C]
+        pixel_shuffle_patches = x.reshape(x.shape[0], -1, x.shape[-1]) # [B, hh, ww, C] to [B, ((hh/2) * (ww/2), 4C]
+        return pixel_shuffle_patches
